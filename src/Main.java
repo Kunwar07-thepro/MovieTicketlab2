6a42bd3045a365dd27b8ddd334d6b09c9a125409
Kunwar Surya Partap <Kunwar07-thepro>
2020-10-01 23:10:15 -0400

    a

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,19 @@
 import com.sun.glass.ui.Application;
-import com.sun.javafx.tk.TKScenePaintListener;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 
-
 import java.util.Scanner;
-public class MovieTicketTestHarness extends Application {
+public class Main extends Application {
     public static void main(String[] args) {
 
 
 
-//
     }
     @Override
     public void start(Stage stage) throws Exception
-    {Parent root  = FXMLLoader.load(getClass().getResource("MovieTicket"));
-    Scene scene = new Scene(root);
-    strage.setScene(scene);
-    stage.setTitle("Movie Ticket Shower");
-    stage.show();
-    }
-
+    {  }
 }
-// MovieTicket movie1= new MovieTicket("Averger",1,34.1);
+//    MovieTicket movie1= new MovieTicket("Averger",1,34.1);
 //    MovieTicket movie2= new MovieTicket("Inception",18,49.7);
 //
 //
